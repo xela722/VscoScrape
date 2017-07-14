5175918f7bc098bd039d208e587b542623567b31
xela722 <alex0722@comcast.net>
2017-07-14 13:05:48 -0400

    Added sate start and end

--- scrape.py
+++ scrape.py
@@ -13,6 +13,7 @@ vs=q.cookies['vs']
 
 names=[]
 times=[]
+date = "null"
 
 metas = bs(q.content,'html.parser').find('meta',property='al:ios:url')
 
@@ -27,6 +28,15 @@ print "Scraping VSCO: %s" % (account)
 r = requests.get(mainUrl, headers=headers)
 imgs = []
 
+for arg in range(0,len(sys.argv)):
+    if sys.argv[arg]=="-ds":
+        date="used"
+        dateStart=sys.argv[arg+1]
+    if sys.argv[arg]=="-de":
+        date="used"
+        dateEnd=sys.argv[arg+1]
+
+
 for i in range(0, len(r.json()['media'])-1):
     try:
         str(imgs.append(r.json()['media'][i]['responsive_url']))
@@ -38,11 +48,29 @@ for i in range(0, len(r.json()['media'])-1):
 
 for x in range(0, len(imgs)-1):
     try:
-        reqUrl = str(imgs[x])
-        use = reqUrl.replace("im.", "http://im.")
+        if date!="null":
+            reqUrl = str(imgs[x])
+            use = reqUrl.replace("im.", "http://im.")
+            if dateEnd in names[x]:    
+                urllib.urlretrieve(str(use), "vsco %s.jpg" % (names[x]))
+                print "%s Downloaded" % (names[x])
+                date="null"
+            else:
+                 continue
+        else:
+            if dateStart in names[x]:
+                reqUrl = str(imgs[x])
+                use = reqUrl.replace("im.", "http://im.")
 
-        urllib.urlretrieve(str(use), "vsco %s.jpg" % (names[x]))
-        print "%s Downloaded" % (names[x])
+                urllib.urlretrieve(str(use), "vsco %s.jpg" % (names[x]))
+                print "%s Downloaded" % (names[x])
+                break
+            else:
+                reqUrl = str(imgs[x])
+                use = reqUrl.replace("im.", "http://im.")
+
+                urllib.urlretrieve(str(use), "vsco %s.jpg" % (names[x]))
+                print "%s Downloaded" % (names[x])
     except:
         print "EXCEPT!!!"
         continue
